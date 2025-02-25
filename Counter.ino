ac54cdb8db37f0bd80144c042af1313655c726fa
Antony Lopez Chuan <antony.lopez4879@alumnos.udg.mx>
2025-02-25 17:27:49 -0600

    Add files via upload
    Original program

---
+++ Counter.ino
@@ -0,0 +1,16 @@
+int number = 0, setPins[8] = {15, 4, 18, 19, 21, 22, 23} , matrixNumbers[10][7] = {{1, 1, 1, 1, 1, 1, 0}, {0, 1, 1, 0, 0, 0, 0}, {1, 1, 0, 1, 1, 0, 1}, {1, 1, 1, 1, 0, 0, 1}, {0, 1, 1, 0, 0, 1, 1}, {1, 0, 1, 1, 0, 1, 1}, {1, 0, 1, 1, 1, 1, 1}, {1, 1, 1, 0, 0, 0, 0}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 0, 1, 1}}; 
+void setup() {
+  pinMode(5, INPUT);
+  for(int i=0; i<7; i++) pinMode(setPins[i], OUTPUT);
+}
+void loop() {
+  while(digitalRead(5)==HIGH);
+  for(int i=0; i<7; i++) digitalWrite(setPins[i],matrixNumbers[number][i]);
+  number = (number+1)%10; 
+  while(digitalRead(5)==LOW);
+}
+
+
+
+
+// Pins {a, b,  c,  d,  e,  f,  g}
